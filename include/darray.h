58391e93e1674276cd6b6e0403dd9f07e8ac5796
myyrakle <sssang97@naver.com>
2019-04-07 22:46:01 +0900

    make to new

--- include/darray.h
+++ include/darray.h
@@ -37,8 +37,8 @@ void declname##_iterator_prev(declname##_iterator*); \
 type declname##_iterator_get(const declname##_iterator*); \
 type* declname##_iterator_get_ptr(declname##_iterator*); \
 bool declname##_iterator_equals(const declname##_iterator*, const declname##_iterator*); \
-/*비멤버 make 함수 선언*/ \
-declname##_iterator make_##declname##_iterator(type*); \
+/*비멤버 new 함수 선언*/ \
+declname##_iterator new_##declname##_iterator(type*); \
 \
 \
 /*실체화되는 배열 유사클래스*/ \
@@ -137,8 +137,8 @@ void declname##_for_each_ptr(declname*, void(*)(type*)); \
 void declname##_for_each_cptr(const declname*, void(*)(const type*)); \
 \
 /*비멤버 함수*/ \
-declname make_##declname (); \
-declname make_##declname##_with (size_t); \
+declname new_##declname (); \
+declname new_##declname##_with (size_t); \
 \
 \
 /*배열 메서드 정의.*/ \
@@ -194,12 +194,12 @@ void declname##_sort(declname* self) \
 \
 void declname##_sort_by(declname* self, int(*comp)(const type*, const type*)) \
 { \
-     qsort(self->data, self->length, sizeof(type), comp); \
+     qsort(self->data, self->length, sizeof(type), (int(*)(const void*, const void*))comp); \
 } \
 \
 declname declname##_clone(const declname* self) \
 { \
-    declname temp = make_##declname##_with(self->length); \
+    declname temp = new_##declname##_with(self->length); \
     for(int i=0;i<self->length;++i) \
         temp.data[i] = self->data[i]; \
     return temp; \
@@ -208,14 +208,14 @@ declname declname##_clone(const declname* self) \
 declname##_iterator declname##_begin(declname* self) \
 { \
     declname##_iterator it = \
-    make_##declname##_iterator(self->data); \
+    new_##declname##_iterator(self->data); \
     return it; \
 } \
 \
 declname##_iterator declname##_end(declname* self) \
 { \
     declname##_iterator it = \
-    make_##declname##_iterator(self->data+self->length); \
+    new_##declname##_iterator(self->data+self->length); \
     return it; \
 } \
 \
@@ -235,7 +235,7 @@ declname##_iterator declname##_find(const declname* self, const type key) \
 { \
     for(int i = 0; i<self->length; ++i)\
         if(self->data[i] == key) \
-            return make_##declname##_iterator((type*)&(self->data[i])); \
+            return new_##declname##_iterator((type*)&(self->data[i])); \
     \
     return self->end((declname*)&self); \
 } \
@@ -244,7 +244,7 @@ declname##_iterator declname##_find_by(const declname* self, int(*comp)(const ty
 { \
     for(int i = 0; i<self->length; ++i)\
         if(comp(&self->data[i])) \
-            return make_##declname##_iterator((type*)&(self->data[i])); \
+            return new_##declname##_iterator((type*)&(self->data[i])); \
 \
     return self->end((declname*)&self); \
 } \
@@ -253,7 +253,7 @@ declname##_iterator declname##_bfind(const declname* self, const type key) \
 { \
     type* p = bsearch(&key, self->data, self->length, sizeof(type), declname##_comparer); \
     if(self->data<=p && p<= &(self->data[self->length-1])) \
-        return make_##declname##_iterator(p); \
+        return new_##declname##_iterator(p); \
     return self->end((declname*)&self); \
 } \
 \
@@ -289,7 +289,7 @@ declname##_iterator declname##_bfind_by(const declname* self, const type* key, i
 { \
     int index = declname##_bsearch(self, key, comp); \
     if(0<=index && index<self->length) \
-        return make_##declname##_iterator((type*)&self->data[index]); \
+        return new_##declname##_iterator((type*)&self->data[index]); \
     return self->end((declname*)&self); \
 } \
 \
@@ -375,7 +375,7 @@ void declname##_for_each_cptr(const declname* self, void(*f)(const type*)) \
         f(&self->data[i]); \
 } \
 \
-declname make_##declname () \
+declname new_##declname () \
 { \
     declname temp = \
     { \
@@ -415,7 +415,7 @@ declname make_##declname () \
     return temp; \
 } \
 \
-declname make_##declname##_with (size_t len) \
+declname new_##declname##_with (size_t len) \
 { \
     declname temp = \
     { \
@@ -482,7 +482,7 @@ bool declname##_iterator_equals(const declname##_iterator* self, const declname#
     return self->ptr == other->ptr; \
 } \
 \
-declname##_iterator make_##declname##_iterator(type* p) \
+declname##_iterator new_##declname##_iterator(type* p) \
 { \
     declname##_iterator it = \
     { \
@@ -494,4 +494,4 @@ declname##_iterator make_##declname##_iterator(type* p) \
         declname##_iterator_equals \
     }; \
     return it; \
-} \
+} 
