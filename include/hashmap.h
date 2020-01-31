950a86373e8696e967a00350bbc5bc35c7b9bbfe
myyrakle <sssang97@naver.com>
2020-01-31 21:10:05 +0900

    declare hashmap structures

--- include/hashmap.h
+++ include/hashmap.h
@@ -4,6 +4,42 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define decl_hashmap(declname, K, V)
+#define decl_hashmap(declname, K, V) \
+\
+struct declname##_node; \
+struct declname##_bucket; \
+struct declname##_bucket_node; \
+struct declname; \
+struct declname##_iterator; \
+typedef struct declname##_node declname##_node; \
+typedef struct declname##_bucket declname##_bucket; \
+typdef struct declname##_bucket_node declname##_bucket_node; \
+typedef struct declname declname; \
+typedef struct declname##_iterator declname##_iterator; \
+\
+struct declname##_node \
+{ \
+    K key; \
+    V value; \
+}; \
+\
+struct declname##_bucket_node \
+{ \
+    declname##_node current; \
+    declname##_bucket_node* next; \
+}; \
+\
+struct declname##_bucket \
+{ \
+    declname##_node* head; \
+    size_t length; \
+}; \
+\
+struct declname \
+{ \
+    declname##_bucket* bucket_list; \
+    size_t bucket_length; \
+}; \
+
 
 #endif
